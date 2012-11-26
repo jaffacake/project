d6cf7def9a7944dd0d27d15e4077701f3c3400e0
unknown <Grant@Grant-PC.(none)>
2012-11-26 16:29:57 +0000

    table prefix added

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,8 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+# Table prefix
+config.active_record.table_name_prefix = "fyp_"
+
 # Initialize the rails application
 Crm::Application.initialize!
