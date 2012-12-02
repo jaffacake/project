d597725de88cf18f6654b5452ce9c4a8f39d1521
unknown <Grant@Grant-PC.(none)>
2012-12-02 17:32:49 +0000

    adding new view

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Crm::Application.routes.draw do
   # The priority is based upon order of creation:
   # first created -> highest priority.
   
-  root :to => 'application'
+  root :to => 'home#index'
 
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
