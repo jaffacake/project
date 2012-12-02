5499e555a82dd25474e168758e512f49611e4196
unknown <Grant@Grant-PC.(none)>
2012-12-02 14:41:06 +0000

    fixing database prefix issues

--- config/environment.rb
+++ config/environment.rb
@@ -1,8 +1,5 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
-# Table prefix
-config.active_record.table_name_prefix = "fyp_"
-
 # Initialize the rails application
 Crm::Application.initialize!
