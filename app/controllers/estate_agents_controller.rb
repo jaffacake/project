a2885066449833803e1b7730969c90412cd5b222
unknown <Grant@Grant-PC.(none)>
2013-02-12 19:51:24 +0000

    Fixing the controller to allow the creation of the address object

--- app/controllers/estate_agents_controller.rb
+++ app/controllers/estate_agents_controller.rb
@@ -25,7 +25,6 @@ class EstateAgentsController < ApplicationController
   # GET /estate_agents/new.json
   def new
     @estate_agent = EstateAgent.new
-    @address = Address.new
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @estate_agent }
