6385979f847c7781f1f3213c3c23eec183a65fdb
unknown <Grant@Grant-PC.(none)>
2013-02-12 19:55:17 +0000

    Adding the attr_accesible setting to the controller, this should fix the errors in the new_estate_agent page

--- app/models/estate_agent.rb
+++ app/models/estate_agent.rb
@@ -2,5 +2,7 @@ class EstateAgent < ActiveRecord::Base
   # attr_accessible :title, :body
   has_one :address
   
+  attr_accessible :id, :estate_agent, :license_expiry_date
+  
   accepts_nested_attributes_for :address, :allow_destroy => :true,
 end
