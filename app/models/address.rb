a77e4a9e512f27e4259ec5c862352b8e1c131ff2
unknown <Grant@Grant-PC.(none)>
2013-02-12 19:58:22 +0000

    Making more fixes to the models

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,7 @@
 class Address < ActiveRecord::Base
   # attr_accessible :title, :body
+  
+  attr_accessible :id, :address_line1, :address_line2, :address_town, :address_city, :address_postcode
+  
+  
 end
--- app/models/estate_agent.rb
+++ app/models/estate_agent.rb
@@ -2,7 +2,7 @@ class EstateAgent < ActiveRecord::Base
   # attr_accessible :title, :body
   has_one :address
   
-  attr_accessible :id, :estate_agent, :license_expiry_date
+  attr_accessible :id, :estate_agent, :license_expiry_date, :address
   
   accepts_nested_attributes_for :address, :allow_destroy => :true,
 end
