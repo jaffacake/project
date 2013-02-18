f93e3415b9a38c576aafb3c4eb08808879237019
unknown <Grant@Grant-PC.(none)>
2013-02-18 11:43:42 +0000

    Fixing issues with the plural of the Address model

--- app/models/estate_agent.rb
+++ app/models/estate_agent.rb
@@ -1,8 +1,9 @@
 class EstateAgent < ActiveRecord::Base
   # attr_accessible :title, :body
-  has_one :address
+  has_one :addresses
   
   attr_accessible :id, :estate_agent, :license_expiry_date, :address
   
-  accepts_nested_attributes_for :address, :allow_destroy => :true,
+  accepts_nested_attributes_for :addresses, :allow_destroy => :true,
+  
 end
