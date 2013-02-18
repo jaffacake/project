0624051f3ea2c4ab58fbe2c974ac59306a55b748
unknown <Grant@Grant-PC.(none)>
2013-02-18 11:54:48 +0000

    Fixed an error where :estate_agent should have been :name. This should fix the error in  the edit page.

--- app/models/estate_agent.rb
+++ app/models/estate_agent.rb
@@ -2,7 +2,7 @@ class EstateAgent < ActiveRecord::Base
   # attr_accessible :title, :body
   has_one :address
   
-  attr_accessible :id, :estate_agent, :license_expiry_date, :address
+  attr_accessible :id, :name, :license_expiry_date, :address
   
   accepts_nested_attributes_for :address, :allow_destroy => :true,
  
