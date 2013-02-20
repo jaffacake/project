c2d08ec6d974ba578a0a1d893f39735e8eb65bd8
unknown <Grant@Grant-PC.(none)>
2013-02-20 15:53:17 +0000

    Added the user nested attribute and relationship to the Estate Agents model

--- app/controllers/estate_agents_controller.rb
+++ app/controllers/estate_agents_controller.rb
@@ -26,6 +26,7 @@ class EstateAgentsController < ApplicationController
   def new
     @estate_agent = EstateAgent.new
     @estate_agent.build_address
+    @estate_agent.build_user
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @estate_agent }
--- app/models/estate_agent.rb
+++ app/models/estate_agent.rb
@@ -1,9 +1,10 @@
 class EstateAgent < ActiveRecord::Base
   # attr_accessible :title, :body
   belongs_to :address
+  has_many :user
   
-  attr_accessible :id, :name, :license_expiry_date, :address_attributes
+  attr_accessible :id, :name, :license_expiry_date, :address_attributes, :user_attributes
   
-  accepts_nested_attributes_for :address, :allow_destroy => :true
+  accepts_nested_attributes_for :user, :address, :allow_destroy => :true
  
 end
