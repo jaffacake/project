9f0470870d117b170952f0d66e31c84b3d51d56c
unknown <Grant@Grant-PC.(none)>
2013-02-23 23:37:09 +0000

    Adding a menu to the application layout.
    Fixing issues with the user logout not working.

--- config/routes.rb
+++ config/routes.rb
@@ -10,9 +10,10 @@ Crm::Application.routes.draw do
     resources :addresses
 
 
+  #resources :users
   devise_for :users
   
-  # The priority is based upon order of creation:
+   # The priority is based upon order of creation:
   # first created -> highest priority.
   
   root :to => 'dashboard#index'
