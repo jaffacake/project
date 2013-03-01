1600bc6399b07e42b5d6eb985376042f153b635c
unknown <Grant@Grant-PC.(none)>
2013-03-01 11:02:52 +0000

    Added many to many relationshiop using through method. Many Landlords can have many Properties

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,7 +2,7 @@ class Property < ActiveRecord::Base
   # attr_accessible :title, :body
   
   belongs_to :address
-  has_many :landlords, :through => :appointments
+  has_many :landlords, :through => :landlords_has_properties
   has_many :tenants
   
 end
